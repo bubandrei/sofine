e3ec9c4132189d9e9db121adb42eae57fe6f2d30
a.bubelev <bubeleu@gmail.com>
2022-12-24 15:51:47 +0300

    links for icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
 import React from "react";
 import './Navbar.css';
 import logoKEUNE from '../assets/logo_KEUNE.svg';
-import logoKEUNEWhite from '../assets/logo_KEUNE_WHITE.svg'
+import logoKEUNEWhite from '../assets/logo_KEUNE_WHITE.svg';
+import fb_WHITE from '../assets/fb_WHITE.svg';
+import twitter_WHITE from '../assets/twitter_WHITE.svg';
+import yt_WHITE from '../assets/yt_WHITE.svg';
+import insta_WHITE from '../assets/insta_WHITE.svg'
 
 const Navbar = () => {
     return (
@@ -34,7 +38,18 @@ const Navbar = () => {
                             <a href="/" className="nav-links">Kontakt</a>
                         </li>
                         <li>
-                            <a href="/" className="nav-links">Icons</a>
+                            <div href="/" className="nav-links row-Item">
+                                <div className="firstRowIcon">
+                                    <a href="http://www.facebook.com"><img className="styleImg" src={fb_WHITE}></img></a>
+                                </div>
+                                <div className="secondRowIcon">
+                                    <a href="http://www.twitter.com" ><img className="styleImg"  src={twitter_WHITE}></img></a>
+                                    <a href="http://www.youtube.com" ><img className="styleImg"  src={yt_WHITE}></img></a>
+                                </div>
+                                <div className="thirdRowItem">
+                                    <a href="http://www.instagram.com" ><img className="styleImg"  src={insta_WHITE}></img></a>
+                                </div>
+                            </div>
                         </li>
                     </ul>
                     {/* <div className="hamburger" onClick={handleClick}>
