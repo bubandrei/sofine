692ffc534711d8217184bb5180496f28edac8845
a.bubelev <bubeleu@gmail.com>
2022-12-25 17:30:21 +0300

    fix carusel

--- src/components/Carusel.js
+++ src/components/Carusel.js
@@ -6,8 +6,8 @@ import storeImg from "./storeImg";
 const Carusel = () => {
     return (
         <MainCarusel>
-            <div className='item item-1'><img src={storeImg.slide1}></img></div>
-            <div className='item item-2'><img src={storeImg.slide2}></img></div>
+            <div className='item item-1'><img className='imgForCarusel' src={storeImg.slide1}></img></div>
+            <div className='item item-2'><img className='imgForCarusel' src={storeImg.slide2}></img></div>
         </MainCarusel>
     );
 }
